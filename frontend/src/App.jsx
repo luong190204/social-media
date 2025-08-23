dce05ef4857903948e63c5dbeb8667f6b1ecab01
Nguyen Dinh Luong <dinhluong19002004@gmail.com>
2025-08-23 22:11:22 +0700

    fit: setup frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+  return (
+    <div className="bg-green-500 text-white p-10">
+      <h1 className="text-3xl font-bold">Hello Tailwind</h1>
+    </div>
+  );
+}
+
+
+export default App
